3b97285cea4c356d93ca68c8ea78fd0e502c2143
Harald August Bergesen <bergesenha@gmail.com>
2017-06-04 21:19:15 +0200

    Add type_for_each and value_for_each on type_list. 	modified:   include/type_list.hpp 	modified:   src/main.cpp

--- include/type_list.hpp
+++ include/type_list.hpp
@@ -90,5 +90,40 @@ struct concat<type_list<Types1...>, type_list<Types2...>>
 
 template <class TypeList1, class TypeList2>
 using concat_t = typename concat<TypeList1, TypeList2>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// apply TypeMetaFunction to each type in TypeList and return type_list of the
+// resulting types, where TypeMetaFunction is a type alias.
+template <class TypeList, template <class> class TypeMetaFunction>
+struct type_for_each;
+
+template <class... Types, template <class> class TypeMetaFunction>
+struct type_for_each<type_list<Types...>, TypeMetaFunction>
+{
+    typedef type_list<TypeMetaFunction<Types>...> type;
+};
+
+template <class TypeList, template <class> class TypeMetaFunction>
+using type_for_each_t =
+    typename type_for_each<TypeList, TypeMetaFunction>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// apply TypeMetaFunction to each type in TypeList and return array of the
+// result of TypeMetaFunction on each type in TypeList.
+template <class TypeList, template <class> class TypeMetaFunction>
+struct value_for_each;
+
+template <class... Types, template <class> class TypeMetaFunction>
+struct value_for_each<type_list<Types...>, TypeMetaFunction>
+{
+    typedef decltype(TypeMetaFunction<head_t<type_list<Types...>>>::value) type;
+    static constexpr type value[] = {TypeMetaFunction<Types>::value...};
+};
+
+template <class... Types, template <class> class TypeMetaFunction>
+constexpr typename value_for_each<type_list<Types...>, TypeMetaFunction>::type
+    value_for_each<type_list<Types...>, TypeMetaFunction>::value[];
 }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <type_list.hpp>
 
+
+template <class T>
+using constify_t = const T;
+
+template <class T>
+struct sizefunc
+{
+    static const std::size_t value = sizeof(T);
+};
+
+using namespace metamusil::t_list;
+
+typedef type_list<int, char, double> mylist;
+
+template <class T>
+void p(T)
+{
+    puts(__PRETTY_FUNCTION__);
+}
 
 int
 main()
 {
-    std::cout << "Hello world\n";
+    p(type_for_each_t<mylist, constify_t>());
+    p(value_for_each<mylist, sizefunc>::type());
+
+    for(auto i : value_for_each<mylist, sizefunc>::value)
+    {
+        std::cout << i << '\n';
+    }
 }
