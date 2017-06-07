d3e5c2135d5147ffd390ddd64ab31b7081e880e1
Harald August Bergesen <bergesenha@gmail.com>
2017-06-07 12:21:25 +0200

    Add unit tests for deduce_parameter_types for member functions. 	modified:   tests/test_function_deduction.cpp

--- tests/test_function_deduction.cpp
+++ tests/test_function_deduction.cpp
@@ -130,3 +130,48 @@ TEST_CASE("test parameter type deduction for free functions",
                               t_list::type_list<const char*, long long>>::value;
     REQUIRE(same5);
 }
+
+
+TEST_CASE("test parameter type deduction for member functions",
+          "[deduce_parameter_types]")
+{
+    typedef deduce_parameter_types_t<decltype(&class1::mfunc1)> param_list1;
+    typedef deduce_parameter_types_t<decltype(&class1::mfunc2)> param_list2;
+    typedef deduce_parameter_types_t<decltype(&class1::mfunc3)> param_list3;
+    typedef deduce_parameter_types_t<decltype(&class1::mfunc4)> param_list4;
+    typedef deduce_parameter_types_t<decltype(&class1::mfunc5)> param_list5;
+
+    auto same1 = std::is_same<param_list1, t_list::type_list<>>::value;
+    REQUIRE(same1);
+    auto same2 = std::is_same<param_list2, t_list::type_list<>>::value;
+    REQUIRE(same2);
+    auto same3 = std::is_same<param_list3, t_list::type_list<double>>::value;
+    REQUIRE(same3);
+    auto same4 =
+        std::is_same<param_list4, t_list::type_list<double, char>>::value;
+    REQUIRE(same4);
+    auto same5 = std::is_same<param_list5,
+                              t_list::type_list<const char*, long long>>::value;
+    REQUIRE(same5);
+
+
+    typedef deduce_parameter_types_t<decltype(&class1::cmfunc1)> cparam_list1;
+    typedef deduce_parameter_types_t<decltype(&class1::cmfunc2)> cparam_list2;
+    typedef deduce_parameter_types_t<decltype(&class1::cmfunc3)> cparam_list3;
+    typedef deduce_parameter_types_t<decltype(&class1::cmfunc4)> cparam_list4;
+    typedef deduce_parameter_types_t<decltype(&class1::cmfunc5)> cparam_list5;
+
+    auto csame1 = std::is_same<cparam_list1, t_list::type_list<>>::value;
+    REQUIRE(csame1);
+    auto csame2 = std::is_same<cparam_list2, t_list::type_list<>>::value;
+    REQUIRE(csame2);
+    auto csame3 = std::is_same<cparam_list3, t_list::type_list<double>>::value;
+    REQUIRE(csame3);
+    auto csame4 =
+        std::is_same<cparam_list4, t_list::type_list<double, char>>::value;
+    REQUIRE(csame4);
+    auto csame5 =
+        std::is_same<cparam_list5,
+                     t_list::type_list<const char*, long long>>::value;
+    REQUIRE(csame5);
+}
