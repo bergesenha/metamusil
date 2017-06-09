ed31685c43053f80ba879c7f0c7d3e47d28c9645
Harald August Bergesen <bergesenha@gmail.com>
2017-06-09 14:02:29 +0200

    Add integer_sequence_from_range and unit tests. 	modified:   include/integer_sequence.hpp 	modified:   tests/test_integer_sequence.cpp

--- include/integer_sequence.hpp
+++ include/integer_sequence.hpp
@@ -21,5 +21,27 @@ struct append<std::integer_sequence<IntType, Nums...>, Num>
 template <class IntSeq, typename IntSeq::value_type Num>
 using append_t = typename append<IntSeq, Num>::type;
 
+
+template <class IntType,
+          IntType Begin,
+          IntType End,
+          class Accum = std::integer_sequence<IntType>>
+struct integer_sequence_from_range
+    : integer_sequence_from_range<IntType,
+                                  Begin + 1,
+                                  End,
+                                  append_t<Accum, Begin>>
+{
+};
+
+template <class IntType, IntType End, class Accum>
+struct integer_sequence_from_range<IntType, End, End, Accum>
+{
+    typedef Accum type;
+};
+
+template <class IntType, IntType Begin, IntType End>
+using integer_sequence_from_range_t =
+    typename integer_sequence_from_range<IntType, Begin, End>::type;
 }
 }
--- tests/test_integer_sequence.cpp
+++ tests/test_integer_sequence.cpp
@@ -40,3 +40,31 @@ TEST_CASE("test int_seq::append", "[int_seq::append]")
         REQUIRE(same);
     }
 }
+
+
+TEST_CASE("test integer_sequence_from_range", "[integer_sequence_from_range]")
+{
+    typedef metamusil::int_seq::integer_sequence_from_range_t<std::size_t, 2, 5>
+        from2until5;
+
+    typedef metamusil::int_seq::integer_sequence_from_range_t<int, 3, 3>
+        from3until3;
+
+    typedef metamusil::int_seq::integer_sequence_from_range_t<int, -2, 3>
+        from_minus2until2;
+
+    typedef metamusil::int_seq::integer_sequence_from_range_t<std::size_t, 4, 5>
+        from4until5;
+
+    auto same = std::is_same<from2until5, std::index_sequence<2, 3, 4>>::value;
+    auto same2 = std::is_same<from3until3, std::integer_sequence<int>>::value;
+    auto same3 =
+        std::is_same<from_minus2until2,
+                     std::integer_sequence<int, -2, -1, 0, 1, 2>>::value;
+    auto same4 = std::is_same<from4until5, std::index_sequence<4>>::value;
+
+    REQUIRE(same);
+    REQUIRE(same2);
+    REQUIRE(same3);
+    REQUIRE(same4);
+}
