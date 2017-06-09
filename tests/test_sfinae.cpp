1492e892b5624a99066ba80cd63e0a3d69bbd0ca
Harald August Bergesen <bergesenha@gmail.com>
2017-06-09 11:54:57 +0200

    Add unit tests for integral_specialization_exists. 	modified:   tests/test_sfinae.cpp

--- tests/test_sfinae.cpp
+++ tests/test_sfinae.cpp
@@ -16,16 +16,46 @@ struct spec_mock<void>
 {
 };
 
+
+template <std::size_t N>
+struct int_spec_mock;
+
+template <>
+struct int_spec_mock<10>
+{
+};
+
+template <>
+struct int_spec_mock<32>
+{
+};
+
 TEST_CASE("test specialization_exists", "[specialization_exists]")
 {
-    auto int_spec_exists =
+    const auto int_spec_exists =
         metamusil::specialization_exists<spec_mock, int>::value;
-    auto double_spec_exists =
+    const auto double_spec_exists =
         metamusil::specialization_exists<spec_mock, double>::value;
-    auto void_spec_exists =
+    const auto void_spec_exists =
         metamusil::specialization_exists<spec_mock, void>::value;
 
     REQUIRE(int_spec_exists);
     REQUIRE(double_spec_exists == false);
     REQUIRE(void_spec_exists);
+}
+
+
+TEST_CASE("test integral_specialization_exists",
+          "[integral_specialization_exists]")
+{
+    const auto spec_10_exists = metamusil::
+        integral_specialization_exists<std::size_t, int_spec_mock, 10ll>::value;
+    const auto spec_20_exists = metamusil::
+        integral_specialization_exists<std::size_t, int_spec_mock, 20ll>::value;
+    const auto spec_32_exists = metamusil::
+        integral_specialization_exists<std::size_t, int_spec_mock, 32ll>::value;
+
+    REQUIRE(spec_10_exists);
+    REQUIRE(spec_20_exists == false);
+    REQUIRE(spec_32_exists);
 }
