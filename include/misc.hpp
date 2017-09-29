e3e8548edb5362c4b6fc0be2a74d97a04e9a9afd
Harald August Bergesen <bergesenha@gmail.com>
2017-09-29 11:26:20 +0200

    Add type metafunction base. 	modified:   include/misc.hpp 	modified:   tests/test_misc.cpp

--- include/misc.hpp
+++ include/misc.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 
 namespace metamusil
 {
@@ -17,4 +19,49 @@ struct negate
     template <class T>
     using type = negative_predicate<T>;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+// strip a type down to its pure base type
+template <class T>
+struct base_
+{
+    typedef T type;
+};
+
+template <class T>
+struct base_<T*> : base_<T>
+{
+};
+
+template <class T>
+struct base_<T* const> : base_<T>
+{
+};
+
+template <class T>
+struct base_<T* volatile> : base_<T>
+{
+};
+
+template <class T>
+struct base_<T* const volatile> : base_<T>
+{
+};
+
+template <class T>
+struct base_<T&> : base_<T>
+{
+};
+
+template <class T>
+struct base_<T&&> : base_<T>
+{
+};
+
+template <class T>
+using base = std::remove_cv<typename base_<T>::type>;
+
+template <class T>
+using base_t = typename base<T>::type;
 }
