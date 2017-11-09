5bd2d14d01c629377e7b57db2b2fe25f6a96091a
Harald August Bergesen <bergesenha@gmail.com>
2017-11-09 11:42:01 +0100

    Add enum class type_tag and classes representing modifiers/qualifiers 	modified:   include/type_stack.hpp

--- include/type_stack.hpp
+++ include/type_stack.hpp
@@ -5,9 +5,39 @@ namespace metamusil
 {
 namespace t_stack
 {
-template <class...>
+// represents a type as its base type and qualifiers/modifiers in a stack
+template <class, class...>
 struct type_stack
 {
+};
+
+
+enum class type_tag
+{
+    pointer_tag,
+    const_tag,
+    lreference_tag,
+    rreference_tag
+};
+
+struct Pointer
+{
+    static const type_tag value = type_tag::pointer_tag;
+};
+
+struct Const
+{
+    static const type_tag value = type_tag::const_tag;
+};
+
+struct LReference
+{
+    static const type_tag value = type_tag::lreference_tag;
+};
+
+struct RReference
+{
+    static const type_tag value = type_tag::rreference_tag;
 };
 }
 }
