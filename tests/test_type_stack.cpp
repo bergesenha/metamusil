d16acb55d0102a91fcde13983121ef6f1a4ea414
Harald August Bergesen <bergesenha@gmail.com>
2017-11-09 12:16:14 +0100

    Add tests.

--- tests/test_type_stack.cpp
+++ tests/test_type_stack.cpp
@@ -1,6 +1,16 @@
 #include "catch.hpp"
 
+#include <type_stack.hpp>
 
-TEST_CASE("","")
+TEST_CASE("decompose types into type_stacks","[type_stack]")
 {
+    using namespace metamusil::t_stack;
+
+    typedef decompose_t<int> int_stack;
+
+
+
+    constexpr auto int_stack_check = std::is_same<int_stack, type_stack<int>>::value;
+
+    CHECK(int_stack_check);
 }
