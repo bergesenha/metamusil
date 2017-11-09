5764eed8b9695f43e1da9192bec43bee4ee8d5fa
Harald August Bergesen <bergesenha@gmail.com>
2017-11-09 11:57:16 +0100

    Add push_tag and decompose metafunctions. 	modified:   include/type_stack.hpp

--- include/type_stack.hpp
+++ include/type_stack.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
+#include <type_traits>
+
 
 namespace metamusil
 {
 namespace t_stack
 {
 // represents a type as its base type and qualifiers/modifiers in a stack
-template <class, class...>
+template <class BaseType, class... Tags>
 struct type_stack
 {
 };
 
 
+// represents qualifiers/modifiers at run time
 enum class type_tag
 {
     pointer_tag,
@@ -20,24 +23,68 @@ enum class type_tag
     rreference_tag
 };
 
-struct Pointer
+
+template <class TypeStack, class Tag>
+struct push_tag;
+
+template <class T, class... Tags, class Tag>
+struct push_tag<type_stack<T, Tags...>, Tag>
 {
-    static const type_tag value = type_tag::pointer_tag;
+    typedef type_stack<T, Tag, Tags...> type;
 };
 
-struct Const
+template <class TypeStack, class Tag>
+using push_tag_t = typename push_tag<TypeStack, Tag>::type;
+
+
+// decompose a given type T into type_stack of the type and tags of modifiers
+// and qualifiers
+template <class T, class TypeStack>
+struct decompose_
 {
-    static const type_tag value = type_tag::const_tag;
+    typedef TypeStack type;
 };
 
-struct LReference
+template <class T, class TypeStack>
+struct decompose_<T*, TypeStack>
+    : decompose_<
+          T,
+          push_tag_t<TypeStack,
+                     std::integral_constant<type_tag, type_tag::pointer_tag>>>
 {
-    static const type_tag value = type_tag::lreference_tag;
 };
 
-struct RReference
+template <class T, class TypeStack>
+struct decompose_<const T, TypeStack>
+    : decompose_<
+          T,
+          push_tag_t<TypeStack,
+                     std::integral_constant<type_tag, type_tag::const_tag>>>
 {
-    static const type_tag value = type_tag::rreference_tag;
 };
+
+template <class T, class TypeStack>
+struct decompose_<T&, TypeStack>
+    : decompose_<T,
+                 push_tag_t<TypeStack,
+                            std::integral_constant<type_tag,
+                                                   type_tag::lreference_tag>>>
+{
+};
+
+template <class T, class TypeStack>
+struct decompose_<T&&, TypeStack>
+    : decompose_<T,
+                 push_tag_t<TypeStack,
+                            std::integral_constant<type_tag,
+                                                   type_tag::rreference_tag>>>
+{
+};
+
+template <class T>
+using decompose = decompose_<T, type_stack<T>>;
+
+template <class T>
+using decompose_t = typename decompose<T>::type;
 }
 }
