794e438280ad47a368418646d8122af18ea55f91
Harald August Bergesen <bergesenha@gmail.com>
2017-11-09 12:40:56 +0100

    Rename type_stack to type_descriptor. 	modified:   include/type_stack.hpp 	modified:   tests/test_type_stack.cpp

--- include/type_stack.hpp
+++ include/type_stack.hpp
@@ -7,9 +7,12 @@ namespace metamusil
 {
 namespace t_stack
 {
+
+
+
 // represents a type as its base type and qualifiers/modifiers in a stack
 template <class BaseType, class... Tags>
-struct type_stack
+struct type_descriptor
 {
 };
 
@@ -32,9 +35,9 @@ template <class TypeStack, class Tag>
 struct push_tag;
 
 template <class T, class... Tags, class Tag>
-struct push_tag<type_stack<T, Tags...>, Tag>
+struct push_tag<type_descriptor<T, Tags...>, Tag>
 {
-    typedef type_stack<T, Tag, Tags...> type;
+    typedef type_descriptor<T, Tag, Tags...> type;
 };
 
 template <class TypeStack, class Tag>
@@ -43,53 +46,6 @@ using push_tag_t = typename push_tag<TypeStack, Tag>::type;
 
 // decompose a given type T into type_stack of the type and tags of modifiers
 // and qualifiers
-template <class T, class TypeStack>
-struct decompose_
-{
-    typedef TypeStack type;
-};
-
-template <class T, class TypeStack>
-struct decompose_<T*, TypeStack>
-    : decompose_<
-          T,
-          push_tag_t<TypeStack,
-                     std::integral_constant<type_tag, type_tag::pointer_tag>>>
-{
-};
-
-template <class T, class TypeStack>
-struct decompose_<const T, TypeStack>
-    : decompose_<
-          T,
-          push_tag_t<TypeStack,
-                     std::integral_constant<type_tag, type_tag::const_tag>>>
-{
-};
-
-template <class T, class TypeStack>
-struct decompose_<T&, TypeStack>
-    : decompose_<T,
-                 push_tag_t<TypeStack,
-                            std::integral_constant<type_tag,
-                                                   type_tag::lreference_tag>>>
-{
-};
-
-template <class T, class TypeStack>
-struct decompose_<T&&, TypeStack>
-    : decompose_<T,
-                 push_tag_t<TypeStack,
-                            std::integral_constant<type_tag,
-                                                   type_tag::rreference_tag>>>
-{
-};
-
-template <class T>
-using decompose = decompose_<T, type_stack<T>>;
-
-template <class T>
-using decompose_t = typename decompose<T>::type;
 
 
 // apply a qualifier/modifier to type from a Tag
@@ -129,14 +85,14 @@ template <class TypeStack>
 struct compose;
 
 template <class T>
-struct compose<type_stack<T>>
+struct compose<type_descriptor<T>>
 {
     typedef T type;
 };
 
 template <class T, class Tag, class... RestTags>
-struct compose<type_stack<T, Tag, RestTags...>>
-    : compose<type_stack<apply_t<T, Tag>, RestTags...>>
+struct compose<type_descriptor<T, Tag, RestTags...>>
+    : compose<type_descriptor<apply_t<T, Tag>, RestTags...>>
 {
 };
 
@@ -149,21 +105,21 @@ template <class TypeStack>
 struct to_type_tag_array;
 
 template <class T, class... Tags>
-struct to_type_tag_array<type_stack<T, Tags...>>
+struct to_type_tag_array<type_descriptor<T, Tags...>>
 {
     static constexpr const type_tag value[] = {Tags::value...};
 };
 
 template <class T, class... Tags>
-constexpr const type_tag to_type_tag_array<type_stack<T, Tags...>>::value[];
+constexpr const type_tag to_type_tag_array<type_descriptor<T, Tags...>>::value[];
 
 template <class T>
-struct to_type_tag_array<type_stack<T>>
+struct to_type_tag_array<type_descriptor<T>>
 {
     static constexpr type_tag* value = nullptr;
 };
 
 template <class T>
-constexpr type_tag* to_type_tag_array<type_stack<T>>::value;
+constexpr type_tag* to_type_tag_array<type_descriptor<T>>::value;
 }
 }
