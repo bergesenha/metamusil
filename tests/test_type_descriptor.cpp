19c5d11fe264c46b3124f4eaa2dd9fe46fe2c6a7
Harald August Bergesen <bergesenha@gmail.com>
2017-11-10 20:50:28 +0100

    Add unit tests. 	modified:   tests/test_type_descriptor.cpp

--- tests/test_type_descriptor.cpp
+++ tests/test_type_descriptor.cpp
@@ -31,4 +31,18 @@ TEST_CASE("decompose types to type_descriptors", "[type_descriptor]")
     CHECK(ptr_const_int_check);
     CHECK(lref_ptr_int_check);
     CHECK(lref_const_ptr_const_int_check);
+
+    SECTION("generate arrays of type_tags from descriptors")
+    {
+        typedef array_from_descriptor<int_desc> int_arr_holder;
+        typedef array_from_descriptor<ptr_const_int_desc>
+            ptr_const_int_arr_holder;
+
+
+        CHECK(int_arr_holder::value == nullptr);
+        REQUIRE(std::extent<decltype(ptr_const_int_arr_holder::value)>::value ==
+              2);
+        CHECK(ptr_const_int_arr_holder::value[0] == type_tag::const_tag);
+        CHECK(ptr_const_int_arr_holder::value[1] == type_tag::pointer_tag);
+    }
 }
