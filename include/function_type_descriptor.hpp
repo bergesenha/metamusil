e6eba607433bd6592450050e0a6915598b638eb5
Harald August Bergesen <bergesenha@gmail.com>
2017-11-17 11:41:17 +0100

    Add compose metafunction. 	modified:   include/function_type_descriptor.hpp

--- include/function_type_descriptor.hpp
+++ include/function_type_descriptor.hpp
@@ -12,5 +12,21 @@ template <class ReturnType, bool Constness, class... ParamTypes>
 struct function_type_descriptor
 {
 };
+
+template <class FuncTypeDesc>
+struct compose;
+
+template <class ReturnType, bool Constness, class... ParamTypes>
+struct compose<function_type_descriptor<ReturnType, Constness, ParamTypes...>>
+{
+    typedef ReturnType type(ParamTypes...);
+};
+
+template <class ReturnType, class... ParamTypes>
+struct compose<function_type_descriptor<ReturnType, true, ParamTypes...>>
+{
+    typedef ReturnType type(ParamTypes...) const;
+};
+
 }
 }
