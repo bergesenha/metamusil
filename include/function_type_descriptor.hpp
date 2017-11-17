e0f3bd8d98215418fadf3e3cc832f0f37380957b
Harald August Bergesen <bergesenha@gmail.com>
2017-11-17 11:47:40 +0100

    Add decomopse metafunction. 	modified:   include/function_type_descriptor.hpp

--- include/function_type_descriptor.hpp
+++ include/function_type_descriptor.hpp
@@ -8,11 +8,14 @@ namespace metamusil
 namespace ft_descriptor
 {
 
+// holds function type information at compile time
 template <class ReturnType, bool Constness, class... ParamTypes>
 struct function_type_descriptor
 {
 };
 
+
+// compose a function type from a function_type_descriptor
 template <class FuncTypeDesc>
 struct compose;
 
@@ -28,5 +31,27 @@ struct compose<function_type_descriptor<ReturnType, true, ParamTypes...>>
     typedef ReturnType type(ParamTypes...) const;
 };
 
+template <class FuncTypeDesc>
+using compose_t = typename compose<FuncTypeDesc>::type;
+
+// decompose a function type to a function_type_descriptor
+template <class FunctionType>
+struct decompose;
+
+
+template <class ReturnType, class... ParamTypes>
+struct decompose<ReturnType(ParamTypes...)>
+{
+    typedef function_type_descriptor<ReturnType, false, ParamTypes...> type;
+};
+
+template <class ReturnType, class... ParamTypes>
+struct decompose<ReturnType(ParamTypes...) const>
+{
+    typedef function_type_descriptor<ReturnType, true, ParamTypes...> type;
+};
+
+template <class FunctionType>
+using decompose_t = typename decompose<FunctionType>::type;
 }
 }
